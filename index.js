9219093a954b33118c82f9ffa60978e609d00999
Lance Smith <lance.matthew.smith@gmail.com>
2019-11-24 15:08:00 -0800

    Setup for deployment

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const port = process.env.PORT || 5000;
 const db = require('./config/keys').mongoURI;
 const passport = require('passport');
 
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({
     extended: false
